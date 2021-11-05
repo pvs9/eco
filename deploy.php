24c106826304cc2a285e1b303b5af64de4ded115
Vladimir Patrikeev <vpatrikeev@wall.black>
2021-11-05 15:45:20 +0300

    Update deploy script, remove package

--- deploy.php
+++ deploy.php
@@ -5,10 +5,10 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'bb-logistic');
+set('application', 'eco');
 
 // Project repository
-set('repository', 'git@gitlab.com:bb-logistic/crm.git');
+set('repository', 'git@github.com:pvs9/eco.git');
 
 set('git_cache', true);
 
@@ -29,22 +29,22 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('bb-logistic.com')
-    ->user('vpdev')
-    ->set('http_user', 'vpdev')
-    ->set('deploy_path', '/var/www/html');
+host('84.201.175.83')
+    ->user('ezk')
+    ->set('http_user', 'ezk')
+    ->set('deploy_path', '/var/www/eco');
 
 // Tasks
 
-task('npm', function () {
-    run('cd {{release_path}} && npm ci && npm run prod');
-});
+//task('npm', function () {
+//    run('cd {{release_path}} && npm ci && npm run prod');
+//});
 
 task('rr-binary', function () {
     run('cd {{release_path}} && vendor/bin/rr get');
 });
 
-after('deploy:vendors', 'npm');
+//after('deploy:vendors', 'npm');
 after('deploy:vendors', 'rr-binary');
 
 
@@ -55,9 +55,9 @@ after('deploy:failed', 'deploy:unlock');
 
 before('deploy:symlink', 'artisan:migrate');
 
-task('rr-restart', function () {
-    /** rr restart подвисает и не завершает деплой */
-    run('sudo supervisorctl restart roadrunner');
-});
-
-after('deploy:symlink', 'rr-restart');
+//task('rr-restart', function () {
+//    /** rr restart подвисает и не завершает деплой */
+//    run('sudo supervisorctl restart roadrunner');
+//});
+//
+//after('deploy:symlink', 'rr-restart');
